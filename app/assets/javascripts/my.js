c16db4519efef05f6d37a916269f1686d7e5f2e0
kynt <sato_hirotoki@cyberagent.co.jp>
2018-08-13 19:56:09 +0900

    ローダーを実装

--- app/assets/javascripts/my.js
+++ app/assets/javascripts/my.js
@@ -1,16 +1,33 @@
 //= require jquery
 //= require jquery_ujs
 //= require jquery.turbolinks
+prepare_load_func = function() {
+  var h = $(window).height();
+  $('#wrap').css('display','none');
+  $('#loader-bg ,#loader').height(h).css('display','block');
+}
+
+display_load_func = function() {
+  $('#loader-bg').delay(900).fadeOut(800);
+  $('#loader').delay(600).fadeOut(300);
+  $('#wrap').css('display', 'block');
+}
 
 $(document).ready(function() {
   $('.keyword').change(function() {
+    prepare_load_func();
+    display_load_func();
     $('#page').val(1);
     $('#submit_btn').submit()
     $("#submit_btn").prop("disabled", true);
   })
   $(document)
-  .ajaxStart(function() { $("#submit_btn").prop("disabled", true); })
-  .ajaxComplete(function() { $("#submit_btn").prop("disabled", false); })
+  .ajaxStart(function() {
+    $("#submit_btn").prop("disabled", true);
+  })
+  .ajaxComplete(function() {
+    $("#submit_btn").prop("disabled", false);
+  })
 })
 
 $(document).ready(function() {
@@ -19,8 +36,12 @@ $(document).ready(function() {
     $("#submit_btn").prop("disabled", true);
   })
   $(document)
-  .ajaxStart(function() { $("#submit_btn").prop("disabled", true); })
-  .ajaxComplete(function() { $("#submit_btn").prop("disabled", false); })
+  .ajaxStart(function() {
+    $("#submit_btn").prop("disabled", true);
+  })
+  .ajaxComplete(function() {
+    $("#submit_btn").prop("disabled", false);
+  })
 })
 
 $(document).ready(function() {
@@ -35,21 +56,34 @@ $(document).ready(function() {
       $(".keyword").prop('disabled', false);
     }
 
+    prepare_load_func();
+    display_load_func();
     $('#submit_btn').submit()
     $("#submit_btn").prop("disabled", true);
   });
   $(document)
-  .ajaxStart(function() { $("#submit_btn").prop("disabled", true); })
-  .ajaxComplete(function() { $("#submit_btn").prop("disabled", false); })
+  .ajaxStart(function() {
+    $("#submit_btn").prop("disabled", true);
+  })
+  .ajaxComplete(function() {
+    $("#submit_btn").prop("disabled", false);
+  })
 })
 
 $(document).ready(function() {
   $(".select_check_box").change(function() {
     $('#page').val(1);
+
+    prepare_load_func();
+    display_load_func();
     $('#submit_btn').submit()
     $("#submit_btn").prop("disabled", true);
   });
   $(document)
-  .ajaxStart(function() { $("#submit_btn").prop("disabled", true); })
-  .ajaxComplete(function() { $("#submit_btn").prop("disabled", false); })
+  .ajaxStart(function() {
+    $("#submit_btn").prop("disabled", true);
+  })
+  .ajaxComplete(function() {
+    $("#submit_btn").prop("disabled", false);
+  })
 })
